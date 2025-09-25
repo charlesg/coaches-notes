f1cb18dc636b686e222227c2a803a0d068b3babd
Charles Gagnon <charlesg@unixrealm.com>
2025-09-24 20:49:27 -0400

    Updated page title and suffix.

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,6 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "TSASRA Coach's Notes",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
